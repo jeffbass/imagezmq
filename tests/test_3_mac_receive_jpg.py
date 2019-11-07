bc58b52278be1c82771baf00c676eb33e2297c56
Jeff Bass <jeffbass42@gmail.com>
2019-11-06 20:31:57 -0800

    Change to np.frombuffer to fix deprecation warning

--- tests/test_3_mac_receive_jpg.py
+++ tests/test_3_mac_receive_jpg.py
@@ -31,7 +31,7 @@ import imagezmq
 image_hub = imagezmq.ImageHub()
 while True:  # show streamed images until Ctrl-C
     rpi_name, jpg_buffer = image_hub.recv_jpg()
-    image = cv2.imdecode(np.fromstring(jpg_buffer, dtype='uint8'), -1)
+    image = cv2.imdecode(np.frombuffer(jpg_buffer, dtype='uint8'), -1)
     # see opencv docs for info on -1 parameter
     cv2.imshow(rpi_name, image)  # 1 window for each RPi
     cv2.waitKey(1)
