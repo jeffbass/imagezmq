871cb202a44483f8b4d0b8fe1b25d4a652ca06db
Jeff Bass <jeffbass42@gmail.com>
2019-11-06 20:26:45 -0800

    Change sender format

--- tests/test_3_rpi_send_jpg.py
+++ tests/test_3_rpi_send_jpg.py
@@ -19,8 +19,8 @@ from imutils.video import VideoStream
 import imagezmq
 
 # use either of the formats below to specifiy address of display computer
-# sender = imagezmq.ImageSender(connect_to='tcp://jeff-macbook:5555')
-sender = imagezmq.ImageSender(connect_to='tcp://192.168.1.190:5555')
+sender = imagezmq.ImageSender(connect_to='tcp://jeff-macbook:5555')
+# sender = imagezmq.ImageSender(connect_to='tcp://192.168.1.190:5555')
 
 rpi_name = socket.gethostname()  # send RPi hostname with each image
 picam = VideoStream(usePiCamera=True).start()
