51493c664a2b227deb63af38171505ea1c21b31d
Jeff Bass <jeffbass42@gmail.com>
2020-02-05 23:24:32 -0800

    Update classifiers

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Natural Language :: English',
-        'License :: OSI Approved :: MIT License'
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
