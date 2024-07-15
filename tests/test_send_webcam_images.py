a4f478f52d16a76e06e186df47d03b53e6d76fcb
Jeff Bass <jeff@yin-yang-ranch.com>
2024-07-15 16:47:19 -0700

    Update test_send_webcam_images.py

--- tests/test_send_webcam_images.py
+++ tests/test_send_webcam_images.py
@@ -1,6 +1,6 @@
 """test_send_webcam_images.py -- send OpenCV webcam image stream.
 
-A Raspberry Pi test program that uses imagezmq to send image frames from the
+A webcam test program that uses imagezmq to send image frames from the
 PiCamera continuously to a receiving program on a Mac that will display the
 images as a video stream.
 
@@ -17,7 +17,6 @@ sender = imagezmq.ImageSender(connect_to='tcp://192.168.86.35:5555')
 
 sender_name = "From Webcam" # name will at top of receiver window
 webcam = VideoCapture(0) # 0 is typical source; try 1 or 2 as needed
-# picam.start()
 time.sleep(2.0)  # allow camera sensor to warm up
 while True:  # send images as stream until Ctrl-C
     ret_code, image = webcam.read()
